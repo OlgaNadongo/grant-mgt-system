d54951c007b401a046a3772fd3897ef2f19bec16
Olga Nadongo <winnienadongo@gmail.com>
2024-01-25 10:16:12 +0300

    adding a db.json file to build a fetch API

--- src/components/Dashboard/Partnerform.js
+++ src/components/Dashboard/Partnerform.js
@@ -1,19 +1,45 @@
-import React from "react";
+import React,{useState} from "react";
 import './Partnerform.css'
 
 function Partnerform(){
+
+    const[formData,setFormData]=useState({
+        name:"",
+        project:"",
+        status:""
+    })
+
+    // handle changes when a value is entered in the form
+
+    function handleInputChange(e){
+       setFormData({
+         ...formData,[e.target.name]:e.target.value
+       })
+    }
+
+    // submitting form components
+
+    function handleSubmit(){
+        e.preventdefault()
+    }
     return(
         <div className='formWrapper'>
             <form className='form'>
                <label>
                 Organization Name:
-                <input type='text'value='name'/>
+                <input type='text' name='name'value={formData.name} onChange={handleInputChange}/>
                </label> 
 
                <label>
                   Project Description:
-                  <input type='text' placeholder="project description"/>
+                  <input type='text' name='project' value={formData.project} onChange={handleInputChange}/>
                </label>
+
+               <label>
+                Project Status:
+                <input type='text' name='status' value={formData.status} onChange={handleInputChange}/>
+               </label>
+
                <label>
                  <input type='submit'/>
                 </label> 
